e5d8679d17884aabb685d693aa4664f1b835f72d
kenyasuncin@gmail.com <kenyasuncin@gmail.com>
2025-05-26 17:18:57 -0600

    iniciar

---
+++ Ejemplopersona.java
@@ -0,0 +1,58 @@
+public class Ejemplopersona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private double peso;
+    private double talla;
+
+    // Métodos set
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setTalla(double talla) {
+        this.talla = talla;
+    }
+
+    // Métodos get
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getTalla() {
+        return talla;
+    }
+}
